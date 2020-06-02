c4641b2a5a766c0a489ae7ed7faa3712e58a2957
kanokenji <aleksandrtarsevich77@gmail.com>
2020-06-02 10:57:21 -0600

    Updates

---
+++ precache-manifest.3759a7d3a56a54dd96aafac37de56e14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc7c6cb35fb3e09fd571fc4d4fbc89c",
+    "url": "/todo-list-auth/index.html"
+  },
+  {
+    "revision": "dbaac47e76f368a974de",
+    "url": "/todo-list-auth/static/css/main.5c210795.chunk.css"
+  },
+  {
+    "revision": "2e937889084ca537a416",
+    "url": "/todo-list-auth/static/js/2.75a26bab.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/todo-list-auth/static/js/2.75a26bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbaac47e76f368a974de",
+    "url": "/todo-list-auth/static/js/main.ddfe3ec2.chunk.js"
+  },
+  {
+    "revision": "4d1cdb2fe541c071c7c4",
+    "url": "/todo-list-auth/static/js/runtime-main.9ddca558.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list-auth/static/media/logo.5d5d9eef.svg"
+  }
+]);
